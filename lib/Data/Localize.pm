bc275a34e0a06fbddba5221fd045ad59a34218b6
lestrrat <lestrrat+github@gmail.com>
2009-08-17 11:38:45 +0900

    version++

--- lib/Data/Localize.pm
+++ lib/Data/Localize.pm
@@ -5,7 +5,7 @@ use Any::Moose 'X::AttributeHelpers';
 use I18N::LangTags ();
 use I18N::LangTags::Detect ();
 
-our $VERSION = '0.00009';
+our $VERSION = '0.00010';
 our $AUTHORITY = 'cpan:DMAKI';
 
 BEGIN {
