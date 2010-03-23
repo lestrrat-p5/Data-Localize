4eda0fb276769b0ea7f811bda452c6f4100139f7
lestrrat <lestrrat+github@gmail.com>
2010-03-23 13:11:04 +0900

    add a storage trait

---
+++ lib/Data/Localize/Trait/WithStorage.pm
@@ -0,0 +1,101 @@
+package Data::Localize::Trait::WithStorage;
+use Any::Moose '::Role';
+use File::Temp ();
+use Data::Localize::Util qw(_alias_and_deprecate);
+
+has storage_class => (
+    is => 'ro',
+    isa => 'Str',
+    default => sub {
+        return '+Data::Localize::Storage::Hash';
+    }
+);
+
+has storage_args => (
+    is => 'ro',
+    isa => 'HashRef',
+    default => sub { +{} }
+);
+
+has lexicon_map => (
+    is => 'ro',
+    isa => 'HashRef[Data::Localize::Storage]',
+    default => sub { +{} },
+);
+
+no Any::Moose;
+
+sub get_lexicon_map {
+    my ($self, $key) = @_;
+    return $self->lexicon_map->{ $key };
+}
+
+sub set_lexicon_map {
+    my ($self, $key, $value) = @_;
+    return $self->lexicon_map->{ $key } = $value;
+}
+
+sub get_lexicon {
+    my ($self, $lang, $id) = @_;
+    my $lexicon = $self->get_lexicon_map($lang);
+    return () unless $lexicon;
+    $lexicon->get($id);
+}
+
+sub set_lexicon {
+    my ($self, $lang, $id, $value) = @_;
+    my $lexicon = $self->get_lexicon_map($lang);
+    if (! $lexicon) {
+        $lexicon = $self->build_storage();
+        $self->set_lexicon_map($lang, $lexicon);
+    }
+    $lexicon->set($id, $value);
+}
+
+sub merge_lexicon {
+    my ($self, $lang, $new_lexicon) = @_;
+
+    my $lexicon = $self->get_lexicon_map($lang);
+    if (! $lexicon) {
+        $lexicon = $self->_build_storage($lang);
+        $self->set_lexicon_map($lang, $lexicon);
+    }
+    while (my ($key, $value) = each %$new_lexicon) {
+        $lexicon->set($key, $value);
+    }
+}
+
+sub _build_storage {
+    my ($self, $lang) = @_;
+
+    my $class = $self->storage_class;
+    my $args  = $self->storage_args;
+    my %args;
+
+    if ($class !~ s/^\+//) {
+        $class = "Data::Localize::Storage::$class";
+    }
+    Any::Moose::load_class($class);
+
+    if ( $class->isa('Data::Localize::Storage::BerkeleyDB') ) {
+        my $dir  = ($args->{dir} ||= File::Temp::tempdir(CLEANUP => 1));
+        return $class->new(
+            bdb_class => 'Hash',
+            bdb_args  => {
+                -Filename => File::Spec->catfile($dir, $lang),
+                -Flags    => BerkeleyDB::DB_CREATE(),
+            }
+        );
+    } else {
+        return $class->new();
+    }
+}
+
+_alias_and_deprecate lexicon_get => 'get_lexicon';
+_alias_and_deprecate lexicon_set => 'set_lexicon';
+_alias_and_deprecate lexicon_map_get => 'get_lexicon_map';
+_alias_and_deprecate lexicon_map_set => 'set_lexicon_map';
+_alias_and_deprecate lexicon_merge => 'merge_lexicon';
+
+1;
+
